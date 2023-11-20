3836ba841d1e43a8192c8ee36ae290a80da956e3
Revanfz <revanfauzi81@gmail.com>
2023-11-20 13:48:42 +0700

    refactor: Fix Client Prisma

--- dist/config/prisma.js
+++ dist/config/prisma.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-const client_1 = require("../../prisma/generated/client");
+const client_1 = require("@prisma/client");
 exports.prisma = new client_1.PrismaClient();
